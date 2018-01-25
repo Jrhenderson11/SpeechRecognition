861fb9cabc3b447fada8a75ec6088e121790777b
James <jrhenderson11@gmail.com>
2018-01-25 13:25:30 +0000

    added java sphinx example code

---
+++ JSpeechRecognition/src/TranscriberDemo.java
@@ -0,0 +1,29 @@
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+
+public class TranscriberDemo {
+
+	public static void main(String[] args) throws Exception {
+
+		Configuration configuration = new Configuration();
+
+		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-uk/en-uk");
+		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-uk/cmudict-en-uk.dict");
+		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-uk/en-uk.lm.bin");
+
+		LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+		// Start recognition process pruning previously cached data.
+		recognizer.startRecognition(true);
+		SpeechResult result = recognizer.getResult();
+		// Pause recognition process. It can be resumed then with
+		// startRecognition(false).
+		recognizer.stopRecognition();
+		
+		while ((result = recognizer.getResult()) != null) {
+			System.out.format("Hypothesis: %s\n", result.getHypothesis());
+		}
+		recognizer.stopRecognition();
+	}
+}
