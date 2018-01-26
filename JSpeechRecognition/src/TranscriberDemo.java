178ac69608ebf86990d5be82abea8d874572cc20
James <jrhenderson11@gmail.com>
2018-01-26 11:15:57 +0000

    fixed Java method and turned off console logging

--- JSpeechRecognition/src/TranscriberDemo.java
+++ JSpeechRecognition/src/TranscriberDemo.java
@@ -1,4 +1,5 @@
 
+import java.util.logging.Logger;
 
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
@@ -8,20 +9,29 @@ public class TranscriberDemo {
 
 	public static void main(String[] args) throws Exception {
 
+		System.out.println("setting up speech recognition");
 		Configuration configuration = new Configuration();
 
-		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-uk/en-uk");
-		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-uk/cmudict-en-uk.dict");
-		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-uk/en-uk.lm.bin");
+		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-		LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+		// <property name="logLevel" value="WARNING"/>
+		Logger cmRootLogger = Logger.getLogger("default.config");
+		cmRootLogger.setLevel(java.util.logging.Level.OFF);
+		String conFile = System.getProperty("java.util.logging.config.file");
+		if (conFile == null) {
+			System.setProperty("java.util.logging.config.file", "ignoreAllSphinx4LoggingOutput");
+		}
+
 		// Start recognition process pruning previously cached data.
+		LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
 		recognizer.startRecognition(true);
 		SpeechResult result = recognizer.getResult();
+		System.out.println("Listening...");
 		// Pause recognition process. It can be resumed then with
 		// startRecognition(false).
-		recognizer.stopRecognition();
-		
+
 		while ((result = recognizer.getResult()) != null) {
 			System.out.format("Hypothesis: %s\n", result.getHypothesis());
 		}
